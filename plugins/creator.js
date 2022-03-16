115cfb0be6027232ea06360b597070d3e161ccd6
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 15:59:51 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '18178659441', 'Shadow - Creador - Oficial', m)}
+this.sendContact(m.chat, '18178659441', 'AGROMOS SP-CREADOR - Oficial', m)}
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
