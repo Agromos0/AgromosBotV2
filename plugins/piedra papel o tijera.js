5c2a07b6e713f3f318dec570d55e4e1f85b1fa2b
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 04:48:35 -0600

    Update piedra papel o tijera.js

--- plugins/piedra papel o tijera.js
+++ plugins/piedra papel o tijera.js
@@ -1,5 +1,5 @@
 let handler = async (m, { text, usedPrefix }) => {
-    let salah = `*_Hola 👋🏻, bienvenido al juego de piedra, papel o tijera_*\n\n*_Opciones Disponibles:_*\n*_piedra, papel o tijera_*\n\n*_${usedPrefix}ppt tijera_*\n\n*_Use las opciones en minusculas como se indica en el ejemplo, no use los _ ni los * para que el Bot lo detecte correctamente_*\n\n_©The Shadow Brokers - Bot_`
+    let salah = `*_Hola 👋🏻, bienvenido al juego de piedra, papel o tijera_*\n\n*_Opciones Disponibles:_*\n*_piedra, papel o tijera_*\n\n*_${usedPrefix}ppt tijera_*\n\n*_Use las opciones en minusculas como se indica en el ejemplo, no use los _ ni los * para que el Bot lo detecte correctamente_*\n\N AGROMOS SP- Bot_`
     if (!text) throw salah
     var astro = Math.random()
 
