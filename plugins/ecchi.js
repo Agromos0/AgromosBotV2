8ce47cc48fb94ec6302b94587a61f75656239e2b
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 03:24:03 -0600

    Update ecchi.js

--- plugins/ecchi.js
+++ plugins/ecchi.js
@@ -5,7 +5,7 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Sexy ❤️‍🔥*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Sexy ❤️‍🔥*", 'AGROMOS SP - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^ecchi$/i
 module.exports = handler
 module.exports = handler
