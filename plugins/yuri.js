ad96fa1c08be1237fd8741dbb974f1ead0742dad
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 05:18:39 -0600

    Update yuri.js

--- plugins/yuri.js
+++ plugins/yuri.js
@@ -2,6 +2,6 @@ let fetch = require('node-fetch')
 let handler  = async (m, { conn, usedPrefix, command }) => {
 heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/yuri2?apikey=ConfuMods`)
 json = await heum.buffer()
-conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
+conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', 'AGROMOS SP - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
 handler.command = /^(yuri)$/i
 module.exports = handler
