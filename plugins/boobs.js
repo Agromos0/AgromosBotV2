629d3c862563c6e189e337d965c9975bf7f0a1a3
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 03:09:06 -0600

    Update boobs.js

--- plugins/boobs.js
+++ plugins/boobs.js
@@ -5,6 +5,6 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Boobs 🍒❤️‍🔥*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Boobs 🍒❤️‍🔥*", 'AGROMOS SP - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(boobs|tetas)$/i
 module.exports = handler
