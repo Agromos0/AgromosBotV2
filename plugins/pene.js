7afe4806ec18c1a8165c6372d6eac0fc06723f14
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 04:46:22 -0600

    Update pene.js

--- plugins/pene.js
+++ plugins/pene.js
@@ -5,6 +5,6 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Pene 🍆*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Pene 🍆*", 'Agromos SP - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(pene)$/i
 module.exports = handler
