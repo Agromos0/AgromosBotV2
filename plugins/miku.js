ed86c7ab656ce6f4b3970355057e0b0ac63c3af0
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 04:30:32 -0600

    Update miku.js

--- plugins/miku.js
+++ plugins/miku.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/miku?apikey=APIKEY`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hatsune Miku*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*Hatsune Miku*', '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(micu|miku)$/i
 
