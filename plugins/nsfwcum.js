17370ec04aa216a1a383bdd98d27a0bfa21e4339
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 04:37:12 -0600

    Update nsfwcum.js

--- plugins/nsfwcum.js
+++ plugins/nsfwcum.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/cum?apikey=apirey`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', 'AGROMOS SP - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(cum|nsfwcum)$/i
 
