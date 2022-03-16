8a48d1ca92c06951fadd57a1884a94314eccdb63
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 03:51:45 -0600

    Update meme3.js

--- plugins/meme3.js
+++ plugins/meme3.js
@@ -5,6 +5,6 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*😂😂😂😂😂*", '©The Shadow Borkers - Bot', '😂 SIGUIENTE 😂', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*😂😂😂😂😂*", '©The AGROMOS SP - Bot', '😂 SIGUIENTE 😂', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(meme3)$/i
 module.exports = handler
