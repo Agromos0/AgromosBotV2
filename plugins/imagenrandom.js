43af3851924584f434681bf1adcfa31768e08318
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 03:36:45 -0600

    Update imagenrandom.js

--- plugins/imagenrandom.js
+++ plugins/imagenrandom.js
@@ -5,6 +5,6 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Random img 👽*", '©The Shadow Borkers - Bot', '👾 SIGUIENTE 👾', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Random img 👽*", 'AGROMOS SP - Bot', '👾 SIGUIENTE 👾', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(imagenrandom)$/i
 module.exports = handler
