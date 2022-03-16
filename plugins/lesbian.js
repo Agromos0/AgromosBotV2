41dfb1c494cc79f9e94203833f8de55ee77a51f6
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 03:48:18 -0600

    Update lesbian.js

--- plugins/lesbian.js
+++ plugins/lesbian.js
@@ -5,6 +5,6 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Lesbians*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Lesbians*", '©The AGROMOS SP - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(imglesbians)$/i
 module.exports = handler
