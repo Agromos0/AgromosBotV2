857f6362c3e70933cef395866f9823e9b8172150
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 04:52:18 -0600

    Update play4.js

--- plugins/play4.js
+++ plugins/play4.js
@@ -3,6 +3,6 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 if (!text) throw `*Formato de uso: ${usedPrefix + command} Nombre de la canción o video*\n*Ejemplo:*\n*${usedPrefix + command} Beret ojala*`
 let res = await fetch("https://api-alc.herokuapp.com/api/download/play-mp4?query="+text+"&apikey=ConfuMods")
 let json = await res.json()
-conn.sendFile(m.chat, json.link, 'error.mp4', `   *Aqui tienes el video :D*\n_The Shadow Brokers - Bot_`, m)}
+conn.sendFile(m.chat, json.link, 'error.mp4', `   *Aqui tienes el video :D*\n_Agromos Sp - Bot_`, m)}
 handler.command = /^(play4)$/i
 module.exports = handler
