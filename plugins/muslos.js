a3024cf39af44b3896f6bdff7a4b7ab14df67a85
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 04:32:01 -0600

    Update muslos.js

--- plugins/muslos.js
+++ plugins/muslos.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 	let url = muslos[Math.floor(Math.random() * muslos.length)]
 	await conn.sendMessage(m.chat, {
 		contentText: '🔥🔥🔥🔥',
-		footerText: '©The Shadow Brokers - Bot',
+		footerText: '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇',
 		buttons: [
 			{ buttonId: '.muslos', buttonText: { displayText: 'SIGUIENTE' }, type: 1 }
 		],
