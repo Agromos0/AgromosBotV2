ec77a360a94fb5afd8baeadd2584b397079b7fa2
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 04:45:36 -0600

    Update panties.js

--- plugins/panties.js
+++ plugins/panties.js
@@ -5,6 +5,6 @@ let json = res.data
 let url = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.send2ButtonImg(m.chat, url, "*Panties*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }}) }
+conn.send2ButtonImg(m.chat, url, "*Panties*", 'AGROMOS SP- Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }}) }
 handler.command = /^(panties)$/i
 module.exports = handler
