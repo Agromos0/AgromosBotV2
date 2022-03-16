53eb0a09b286739f51c391d0e9ddc9536353c23e
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 03:29:06 -0600

    Update futbol.js

--- plugins/futbol.js
+++ plugins/futbol.js
@@ -5,6 +5,6 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Fútbol 🥅*", '©The Shadow Borkers - Bot', '⚽ SIGUIENTE ⚽', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Fútbol 🥅*", 'AGROMOS SP - Bot', '⚽ SIGUIENTE ⚽', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(futbol|fútbol)$/i
 module.exports = handler
