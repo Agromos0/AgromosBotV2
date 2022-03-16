cd1328eb91a8c7f5a261f2e80fd460313b160e0c
Agromos0 <101366474+Agromos0@users.noreply.github.com>
2022-03-16 04:32:48 -0600

    Update navidad.js

--- plugins/navidad.js
+++ plugins/navidad.js
@@ -5,6 +5,6 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Navidad 🧑‍🎄*", '©The Shadow Borkers - Bot', '🎄 SIGUIENTE 🎄', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Navidad 🧑‍🎄*", 'Agromos SP - Bot', '🎄 SIGUIENTE 🎄', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(navidad)$/i
 module.exports = handler
