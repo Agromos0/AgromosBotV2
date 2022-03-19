90b1db5ffdbd843aebfa02304b50a3ab3cdd25b6
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-18 22:03:50 -0600

    Update random_dog.js

--- plugins/random_dog.js
+++ plugins/random_dog.js
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, text }) => {
     let json = await res.json()
     if (json.status) throw json
     let caption = `
-©The Shadow Brokers - Bot
+©agromos sp - Bot
 `.trim()
     conn.sendFile(m.chat, json.url, 'dog.jpg', caption, m)
    } catch (e) {
