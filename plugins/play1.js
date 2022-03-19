47a2199976a5fa966283b4fc982aa961c0365ddc
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-18 21:06:08 -0600

    Update play1.js

--- plugins/play1.js
+++ plugins/play1.js
@@ -2,7 +2,7 @@ let yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text }) => {
-if (!text) throw '*✳️ Inserte el nombre/título del video o audio a bucar*\n\n*Ejemplo:*\n*#play Juan Solo - Querido Corazón*'
+if (!text) throw '*✳️ Inserte el nombre/título del video o audio a bucar*\n\n*Ejemplo:*\n*#play Santa-grifa*'
 let results = await yts(text)
 let vid = results.all.find(video => video.seconds < 3600)
 if (!vid) throw '*Video/Audio No encontrado* '
