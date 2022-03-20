5cf7ec7f13485959545ccd63bcaf4ee0f6966966
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 04:40:52 -0600

    Update lolivid.js

--- plugins/lolivid.js
+++ plugins/lolivid.js
@@ -4,6 +4,6 @@ return Math.floor(Math.random() * url)}
 let handler  = async (m, { conn, usedPrefix, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonVid(m.chat, `https://recoders-area.caliph.repl.co/api/lolivid`, "*Loli 😎*", '©The Shadow Borkers - Bot', '😵‍💫 SIGUIENTE 😵‍💫', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonVid(m.chat, `https://recoders-area.caliph.repl.co/api/lolivid`, "*Loli 💚*", 'Agromos SP - Bot', '😳 SIGUIENTE 😳', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(lolivid|lolivideo)$/i
 module.exports = handler
