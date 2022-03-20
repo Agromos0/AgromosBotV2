49eea182515a5570ebb8d13720d61894bdc306e0
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 04:59:28 -0600

    Update logo romanticdouble.js

--- plugins/logo romanticdouble.js
+++ plugins/logo romanticdouble.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#romanticdouble Shadow*'        
+if (!args[0]) throw '*[✔] Ingrese un texto*\n*Ejemplo:*\n*#romanticdouble agromos*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/romantic-double?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
 handler.command = /^(romanticdouble)$/i
