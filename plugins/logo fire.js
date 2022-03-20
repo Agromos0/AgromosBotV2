2e2a800c66c38016fe2b63c7f8a4c2552d3fa47e
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 04:55:55 -0600

    Update logo fire.js

--- plugins/logo fire.js
+++ plugins/logo fire.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#logofire Shadow*'        
+if (!args[0]) throw '*[✔] Ingrese un texto*\n*Ejemplo:*\n*#logofire agromos*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/burn-paper?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
 handler.command = /^(logofire|fire)$/i
