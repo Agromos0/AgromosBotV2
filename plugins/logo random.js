849eb1302eceac583215cf711095c72c9ad68eff
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 04:58:52 -0600

    Update logo random.js

--- plugins/logo random.js
+++ plugins/logo random.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, usedPrefix, command, text }) => {
 response = args.join(' ').split('|')
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#logorandom Shadow*'        
+if (!args[0]) throw '*[✔] Ingrese un texto*\n*Ejemplo:*\n*#logorandom Agromos*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/${pickRandom(global.logos)}?texto=${response[0]}&apikey=ConfuMods`
 conn.sendButtonImg(m.chat, res, "*Logo random terminado*", '©The Shadow Borkers - Bot', 'SIGUIENTE', `${usedPrefix + command} ${text}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(logorandom)$/i
