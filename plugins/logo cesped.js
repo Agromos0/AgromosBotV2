b344ea5d444b2adba7b206bb47b66fb09379387b
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 04:54:30 -0600

    Update logo cesped.js

--- plugins/logo cesped.js
+++ plugins/logo cesped.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#logocesped Shadow*'        
+if (!args[0]) throw '*[✔] Ingrese un texto*\n*Ejemplo:*\n*#logocesped Agromos*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/under-grass?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
 handler.command = /^(undergrass|logocesped|cesped)$/i
