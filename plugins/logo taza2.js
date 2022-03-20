141dd2f8db252b0836dfe3374e1804a248249a07
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 04:53:12 -0600

    Update logo taza2.js

--- plugins/logo taza2.js
+++ plugins/logo taza2.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#logotaza2 Shadow*'        
+if (!args[0]) throw '*[✔] Ingrese un texto*\n*Ejemplo:*\n*#logotaza2 agromos*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/funny-cup?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
 handler.command = /^(funnycup|logotaza2|taza2)$/i
