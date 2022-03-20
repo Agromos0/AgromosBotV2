9c82535e8c6d002c9ef1769ae03a9bf5c37e319f
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 05:03:01 -0600

    Update logo technology.js

--- plugins/logo technology.js
+++ plugins/logo technology.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#technology Shadow*'        
+if (!args[0]) throw '*[✔] Ingrese un texto*\n*Ejemplo:*\n*#technology agromos*'        
 let res = `https://api-alc.herokuapp.com/api/textpro/futuristic-technology?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
 handler.command = /^(technology)$/i
