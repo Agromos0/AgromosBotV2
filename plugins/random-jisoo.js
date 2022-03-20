aeedad3d35b853ef3958a5e82e9d37e650e0ed5c
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 05:35:18 -0600

    Create random-jisoo.js

---
+++ plugins/random-jisoo.js
@@ -0,0 +1,8 @@
+let fetch = require('node-fetch')
+     let handler  = async (m, { conn, usedPrefix, command }) => {
+    heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/jiso?apikey=APIKEY`)
+    json = await heum.buffer()
+   conn.sendButtonImg(m.chat, json, '🌸 *vive la vida como tu quieras*', 'agromos', '🔄 SIGUIENTE', `${usedPrefix + command}`, m, false)
+}
+handler.command = /^(jiso|jisso|jisoo|kimjisoo|)$/i
+module.exports = handler
