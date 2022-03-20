cb520669c2200abd3348516e6fe65acce2911f32
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 04:55:16 -0600

    Update logo coffe.js

--- plugins/logo coffe.js
+++ plugins/logo coffe.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#coffe Shadow*'        
+if (!args[0]) throw '*[✔] Ingrese un texto*\n*Ejemplo:*\n*#coffe agromos*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/coffee-cup?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
 handler.command = /^(coffe|logocoffe)$/i
