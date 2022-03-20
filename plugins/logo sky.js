f717cf7825c43cfc5b60806b7784755fcc90f73f
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 05:01:56 -0600

    Update logo sky.js

--- plugins/logo sky.js
+++ plugins/logo sky.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#logosky Shadow*'        
+if (!args[0]) throw '*[✔] Ingrese un texto*\n*Ejemplo:*\n*#logosky agromos*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/shadow-sky?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
 handler.command = /^(logosky)$/i
