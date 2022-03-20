8574fc81d929bb8daa31bac8aab01e4b4bfd546e
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-20 04:25:22 -0600

    Update boost.js

--- plugins/boost.js
+++ plugins/boost.js
@@ -2,7 +2,7 @@ let { performance } = require('perf_hooks')
 
 let handler = async (m, { conn }) => {
 
- let start = `Empezando aceleración... aguarde un momento`
+ let start = `♦️Empezando aceleración... aguarde un momento♦️`
  let boost = `${pickRandom(['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])}%`
  let boost2 = `${pickRandom(['21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}%`
  let boost3 = `${pickRandom(['41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60'])}%`
@@ -18,7 +18,7 @@ let handler = async (m, { conn }) => {
    let old = performance.now()
    let neww = performance.now()
    let speed = `${neww - old}`
-   let finish = `*_✔️ Bot acelerado con éxito_*\n\n*_Acelerado: ${speed} segundos!_*`
+   let finish = `*_⚡ Bot Agromos SP acelerado con éxito_*\n\n*_Acelerado: ${speed} segundos!_*`
 
      conn.reply(m.chat, finish, m)
 }
