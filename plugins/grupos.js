47e6aa12e4ebaf7f1bd49f046bb0707db6d9d734
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-28 13:43:12 -0600

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -3,10 +3,10 @@ let info = `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de AGROMOS SP - Bot*
 
 *➤ Grupo oficial del Bot:*
-*1.-https://chat.whatsapp.com/JiONzKmPvB25su65XzAbqA
+*1.-https://chat.whatsapp.com/CBoAGmEFcCy44xIoWBSgzJ
 
 *➤ Grupo2 oficial del Bot:*
-*2.-https://chat.whatsapp.com/BzLd8kaNMj24Hmu95tsiE2
+*2.-https://chat.whatsapp.com/FZsoX7iGBZLBQgFX9MUXIy
 `.trim() 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 AGROMOS SP - BOT 🔥*', 'status@broadcast')}
 handler.command = /^(grupos|gruposofc|gruposofc)$/i
