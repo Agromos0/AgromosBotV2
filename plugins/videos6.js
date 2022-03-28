28723b010ca607aecf8d51517aa562cf65525d53
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-27 23:48:01 -0600

    Create videos6.js

---
+++ plugins/videos6.js
@@ -0,0 +1,29 @@
+let handler = async(m, { conn, usedPrefix, command }) => {
+await conn.sendButtonVid(m.chat, pickRandom(asupan), '🔥🔥🔥🔥', 'AgromosBot', 'SIGUIENTE 🔄🥵', `${usedPrefix + command}`, m, false)
+}
+handler.command = /^(pornoaleatorio|pornopremium|pornoalevid|pornovidale|pornopremiumvid|pornopremiumv|Pornopremium|Pornopremiumvid)$/i
+module.exports = handler
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+const asupan = [
+"https://k.top4top.io/m_2277tg6m70.mp4",
+"https://d.top4top.io/m_2277t2jeh0.mp4",
+"https://c.top4top.io/m_2277wxhle0.mp4",
+"https://g.top4top.io/m_22776umn92.mp4",
+"https://e.top4top.io/m_22776amjl4.mp4",
+"https://h.top4top.io/m_2277ksf281.mp4",
+"https://f.top4top.io/m_2277iliie1.mp4",
+"https://c.top4top.io/m_2277axzz01.mp4",
+"https://c.top4top.io/m_22777p6uz0.mp4",
+"https://e.top4top.io/m_22774k28n2.mp4",
+"https://l.top4top.io/m_2277dz2zw2.mp4",
+"https://b.top4top.io/m_2277uaciz3.mp4",
+"https://j.top4top.io/m_2277conhn3.mp4",
+"https://k.top4top.io/m_2277dgyml1.mp4",
+"https://k.top4top.io/m_2277on1gg2.mp4",
+"https://h.top4top.io/m_22774jf141.mp4",
+"https://b.top4top.io/m_22774wjs32.mp4",
+"https://f.top4top.io/m_2277k9gw00.mp4",
+]
