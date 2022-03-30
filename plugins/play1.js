b9ccd6f38d7bf9c35658dd1bc55bc27ae7e86757
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-03-30 14:02:31 -0600

    Update play1.js

--- plugins/play1.js
+++ plugins/play1.js
@@ -1,16 +1,16 @@
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 let yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text }) => {
-if (!text) throw '*✳️ Inserte el nombre/título del video o audio a bucar*\n\n*Ejemplo:*\n*#play Juan Solo - Querido Corazón*'
+if (!text) throw '*✳️ Inserte el nombre/título del video o audio a bucar*\n\n*Ejemplo:*\n*#play Santa-grifa*'
 let results = await yts(text)
 let vid = results.all.find(video => video.seconds < 3600)
 if (!vid) throw '*Video/Audio No encontrado* '
 let isVideo = /2$/.test(command)
 let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4')
 conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*⏯ ️𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇 ⏯️*
+
 *${title}*
 *⇄ㅤ     ◁   ㅤ  ❚❚ㅤ     ▷ㅤ     ↻*
 
@@ -21,7 +21,8 @@ let _thumb = {}
 try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
 catch (e) { }
 conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*🔥Título: ${title}*
+*🔥Título:* ${title}
+*📂Tamaño del archivo:* ${filesizeF}
 `.trim(), m, false, _thumb || {})}
 handler.help = ['play' , 'play2']
 handler.command = /^play2?$/i
