ecf8b86b757b0cc0dbc2e8524d5fa56f911fa5c8
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-01 17:51:32 -0600

    Update menu juegos.js

--- plugins/menu juegos.js
+++ plugins/menu juegos.js
@@ -44,7 +44,7 @@ let menu =`
 ┗━━━━━━━━━━━━━┛
 ━〘 💚⚡💚⚡💚⚡💚⚡💚 〙━`.trim()
 let mentionedJid = [who]
-conn.send3ButtonImg(m.chat, pp, menu, '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', 'HOLA', `Hola`, '𝙼𝙴𝙽𝚄 INFORMACIÓN ', `#menuinformacion`, '𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙸𝙲𝙸𝙰𝙻', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
+conn.send3ButtonImg(m.chat, pp, menu, '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', 'HOLA', `Hola`, '𝙼𝙴𝙽𝚄 INFORMACIÓN ', `#minformacion`, '𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙸𝙲𝙸𝙰𝙻', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
 await await await await await await conn.sendFile(m.chat, vn, 'juegos-bot.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
