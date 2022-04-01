5ce634eac27fd0f39697dfbe4f79a20e4aad17ec
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-01 15:34:57 -0600

    Update menu grupos.js

--- plugins/menu grupos.js
+++ plugins/menu grupos.js
@@ -1,4 +1,4 @@
-//NO funciona este comando nada que editar aquí vale gracias xd
+//7W7 PORFAVOR  DEJAME CREDITOS NO SEAS MALO XD
 let fs = require('fs')
 let fetch = require('node-fetch')
 let moment = require('moment-timezone')
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 let pp = './Menu2.jpg'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let username = conn.getName(who)
-let vn = './media/menu-agromos-bot.mp3'
+//let vn = './media/menu-agromos-bot.mp3'
 let menu =`
 ╭━〘 💚⚡️💚⚡️💚⚡️💚⚡️💚 〙━╮
  ‖    ⚡ _*AGROMOS BOT SP_* ⚡
@@ -47,7 +47,7 @@ let menu =`
 ━〘 💚⚡💚⚡💚⚡💚⚡💚 〙━`.trim()
 let mentionedJid = [who]
 conn.send3ButtonImg(m.chat, pp, menu, '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', '𝙼𝙴𝙽𝚄 𝚂𝙸𝙼𝙿𝙻𝙴', `#menusimple`, '𝙼𝙴𝙽𝚄 𝙰𝚄𝙳𝙸𝙾𝚂', `#menuaudios`, '𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙸𝙲𝙸𝙰𝙻', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
-await await await await await await conn.sendFile(m.chat, vn, 'menu-agromos-bot.mp3', null, m, true, {
+//await await await await await await conn.sendFile(m.chat, vn, 'menu-agromos-bot.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
