a94ca06221b3a58b9c30927972847c6f1d6d57d6
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-01 13:33:11 -0600

    Update yaoi1.js

--- plugins/yaoi1.js
+++ plugins/yaoi1.js
@@ -2,6 +2,6 @@ let fetch = require('node-fetch')
 let handler  = async (m, { conn, usedPrefix, command }) => {
 heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/yaoi?&apikey=ConfuMods`)
 json = await heum.buffer()
-conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
+conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', 'agromos sp - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
 handler.command = /^(yaoi)$/i
 module.exports = handler
