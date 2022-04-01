8d3e25504b9b600d9856c8f9709c461ff5847091
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-01 13:38:26 -0600

    Update wpanime.js

--- plugins/wpanime.js
+++ plugins/wpanime.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn }) => {
         let res = await fetch(global.API('xteam', '/randomimage/wpmobile', {}, 'APIKEY'))
         let img = await res.buffer()
         conn.sendMessage(m.chat, img, MessageType.image, {
-            quoted: m, caption: '*✨The Shadow Brokers - Bot✨*'
+            quoted: m, caption: '*💚Agromos - Bot💚*'
         })
     } catch (e) {
         console.log(e)
