1a8dab77233fa9636a7e8061468224ddf4746d0c
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-01 19:24:07 -0600

    Update and rename menu nuevo.js to menu nuevos.js

--- plugins/menu nuevos.js
+++ plugins/menu nuevos.js
@@ -3,17 +3,17 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 let moment = require('moment-timezone')
 let path = require('path')
-let util = require('util')
+"let util = require('util')"
 let handler = async (m, { conn, usedPrefix }) => {
 let pp = './Menu2.jpg'
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.senderlet
 let username = conn.getName(who)
-let vn = './media/atajos_bot.mp3'
+let vn = './media/creaciones-bot.mp3'
 let menu =`
 ╭━〘 💚⚡️💚⚡️💚⚡️💚⚡️💚 〙━╮
  ‖    ⚡ _*AGROMOS BOT SP_* ⚡
  ‖ 💚*¡𝗛ola! ${username}* 💚
- ‖
+ 
  ‖ Ⓜ️ *_MENÚ ATAJOS_* Ⓜ️
  ┏━━━━━━━━━━━━━┓
 ┃ *<GESTION DE ATAJOS ALOS COMANDOS/>* 
@@ -43,7 +43,7 @@ let menu =`
 ━〘 💚⚡💚⚡💚⚡💚⚡💚 〙━`.trim()
 let mentionedJid = [who]
 conn.send3ButtonImg(m.chat, pp, menu, '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', '𝙼𝙴𝙽𝚄 RANDOM, `#menuramdom`, '𝙼𝙴𝙽𝚄 JUEGOS', `#menujuegos`, '𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙸𝙲𝙸𝙰𝙻', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
-await await await await await await conn.sendFile(m.chat, vn, 'atajos_bot.mp3', null, m, true, {
+await await await await await await conn.sendFile(m.chat, vn, 'creaciones-bot.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
