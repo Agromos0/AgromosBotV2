cab077fb820f52d46758138d630c43f2b6aaff8a
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-01 19:44:29 -0600

    Update menu atajos.js

--- plugins/menu atajos.js
+++ plugins/menu atajos.js
@@ -39,12 +39,9 @@ let menu =`
 ┃ *MENU DE* _CREACIONES_
 ┣ ➱♨️ _${usedPrefix}menucreaciones
 ┗━━━━━━━━━━━━━┛
-━〘 💚⚡💚⚡💚⚡💚⚡💚 ━
-
-┗━━━━━━━━━━━━━┛
 ━〘 💚⚡💚⚡💚⚡💚⚡💚 〙━`.trim()
 let mentionedJid = [who]
-conn.send3ButtonImg(m.chat, pp, menu, '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', '𝙼𝙴𝙽𝚄 𝚂𝙸𝙼𝙿𝙻𝙴', `#menusimple`, '𝙼𝙴𝙽𝚄 𝙰𝚄𝙳𝙸𝙾𝚂', `#menuaudios`, '𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙸𝙲𝙸𝙰𝙻', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
+conn.send3ButtonImg(m.chat, pp, menu, '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', '𝙼𝙴𝙽𝚄 CREADOR', `#menucreador`, '𝙼ENU GRUPOS', `#menugrupos`, 'MENU DESCARGAS', `#menudescargas`, m, false, { contextInfo: { mentionedJid }})   
 await await await await await await conn.sendFile(m.chat, vn, 'atajos-bot.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
