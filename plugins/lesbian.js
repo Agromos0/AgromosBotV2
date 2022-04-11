357c6531810393cc8cc03d5b9e06ea54ff56f242
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-11 04:46:12 -0500

    Update lesbian.js

--- plugins/lesbian.js
+++ plugins/lesbian.js
@@ -1,12 +1,24 @@
 const axios = require('axios')
-if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
+ let handler = async(m, { conn }) => {
+ if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/lesbians')
+            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
+  }
+handler.help = ['imglesbians']
+handler.tags = ['images']
+handler.command = /^(verlesbianas|imagenlesbians)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = false
 
-let handler = async(m, { conn, usedPrefix, command }) => {
-let res = await axios("https://meme-api.herokuapp.com/gimme/lesbians")
-let json = res.data
-let ShadowBot = json.url
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Lesbians*", '©The AGROMOS SP - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
-handler.command = /^(imglesbians)$/i
-module.exports = handler
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = false
+
+module.exports = handler 
