6dd69a7f420bc8ca1a06cb2b481be116213ce016
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-11 05:05:59 -0500

    Update porno.js

--- plugins/porno.js
+++ plugins/porno.js
@@ -3,6 +3,7 @@ let handler = async(m, { conn }) => {
 if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
 let porn = await axios.get('https://meme-api.herokuapp.com/gimme/nsfw')
            conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}`, m) 
+await conn.sendButtonVid(m.chat, pickRandom(asupan), '🔥🔥🔥🔥', 'AgromosBot', 'SIGUIENTE 🔄🥵', `${usedPrefix + command}`, m, false)
   }
 handler.help = ['porno']
 handler.tags = ['images']
