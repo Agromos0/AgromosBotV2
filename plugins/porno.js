562f1c308481275d73281dc217cc18de46a82dac
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-11 05:02:32 -0500

    Update porno.js

--- plugins/porno.js
+++ plugins/porno.js
@@ -1,14 +1,25 @@
+
 const axios = require('axios')
+let handler = async(m, { conn }) => {
 if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
 let porn = await axios.get('https://meme-api.herokuapp.com/gimme/nsfw')
            conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}`, m) 
   }
-let handler = async(m, { conn, usedPrefix, command }) => {
-let res = await axios("https://meme-api.herokuapp.com/gimme/nsfw")
-let json = res.data
-let ShadowBot = json.url
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*🥵🥵🥵🥵🥵*", '𝐴𝐺𝑅𝑂𝑀𝑂𝑆 𝑆𝑃-𝐵𝑂𝑇', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+handler.help = ['porno']
+handler.tags = ['images']
 handler.command = /^(porno)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = false
+
 module.exports = handler
