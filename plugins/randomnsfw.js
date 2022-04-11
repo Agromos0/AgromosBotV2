1f11079b193c066a4eb963efa777322b0dc44756
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-11 05:17:30 -0500

    Update randomnsfw.js

--- plugins/randomnsfw.js
+++ plugins/randomnsfw.js
@@ -1,14 +1,12 @@
 const axios = require('axios')
-//if (isBanned) return  reply(mess.banned)
-//if (!isGroup) return reply('esta función es solo para grupos')
-let handler = async(m, { conn }) => {
-if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
+ let handler = async(m, { conn }) => {
+const axios = require('axios')let handler = async(m, { conn }) => {if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'let porn = await axios.get('https://meme-api.herokuapp.com/gimme/nsfw') conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}`, m) }
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/nsfw')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }
 handler.help = ['randomnsfw']
 handler.tags = ['images']
-handler.command = /^(contenido18|18contenido)$/i
+handler.command = /^(randomnsfw)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
