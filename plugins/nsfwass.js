92de3c4b8a8cc292a2674373a9b0311397b562e6
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-11 06:13:01 -0500

    Update nsfwass.js

--- plugins/nsfwass.js
+++ plugins/nsfwass.js
@@ -1,5 +1,6 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
+     if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/ass?apikey=apirey`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', 'AGROMOS SP- Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
