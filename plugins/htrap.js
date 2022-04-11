afb493082740fa042e508441aa2a61ce139c65c8
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-11 06:01:39 -0500

    Update htrap.js

--- plugins/htrap.js
+++ plugins/htrap.js
@@ -1,10 +1,11 @@
 let axios = require("axios")
 let handler = async (m, { conn, usedPrefix, command }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
 let res = await axios("https://api.waifu.pics/nsfw/trap")
 let json = res.data
 let url = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.send2ButtonImg(m.chat, url, "*UN TRAPITO <3*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }}) }
+conn.send2ButtonImg(m.chat, url, "*UN TRAPITO <3*", 'Agromos sp - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }}) }
 handler.command = /^htrap$/i
 module.exports = handler
