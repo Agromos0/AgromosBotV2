14f88148ac30d7cca45f73f7e09c50c33102ce86
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-12 02:14:15 -0500

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -2,7 +2,7 @@ let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let fetch = require('node-fetch')
 let handler = async (m, { args, usedPrefix, command }) => {
 
-    if (!args[0]) throw 'link githubnya mana? contoh: https://github.com/Bintang73/botst4rz'
+    if (!args[0]) throw 'link  del repositorio githud que quieres clonar ejemplo #gitclone https://github.com/Agromos0/AgromosBotV'
 
     if (!regex.test(args[0])) throw 'link salah!'
 
@@ -11,7 +11,7 @@ let handler = async (m, { args, usedPrefix, command }) => {
     let url = `https://api.github.com/repos/${user}/${repo}/zipball`
     let filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
     // 'attachment; filename=Nurutomo-wabot-aq-v2.5.1-251-g836cccd.zip'
-    m.reply(`*Mohon tunggu, sedang mengirim repository..*`)
+    m.reply(`*Espere un momento mientras envío si repositorio..*`)
     conn.sendFile(m.chat, url, filename, null, m)
 
 }
