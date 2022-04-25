23c7094edd493af967ccde06d08d0f10e0af93a6
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2022-04-24 23:50:04 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,11 @@
 function handler(m) {
-this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '523931806576', 'AGROMOS SP OFICIAL', m)}
-handler.command = /^(contacto|agromos|creator|creador|propietario|dueño)$/i
+  this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
+  this.sendContact(m.chat, '523931806576', 'NUMERO SOPORTE - AGROMOS SP OFICIAL', m)
+  this.sendContact(m.chat, '595983186566', 'OFICIAL - EL SANTI EX ACTOR NOPOR', m)
+  }
+handler.help = ['contacto']
+handler.tags = ['info']
+ 
+handler.command = /^(contacto|owner|creator|creador|propietario|dueño|dueña)$/i
+
 module.exports = handler
